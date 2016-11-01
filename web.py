51ba91bdc2512816359d70612d8531818025065d
LiRuomiao@MBP <liruomiao@gmail.com>
2016-11-02 01:26:50 +0800

    login.html

--- web.py
+++ web.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-  pass
+  return render_template('login.html')
 
 @app.route('/login')
 def login():
@@ -25,3 +25,8 @@ def object():
 @app.route('/filter')
 def filter():
   pass
+
+if __name__ == "__main__":
+  # app = create_app(config="config.yaml")
+  # app.run(use_debugger=use_debugger, debug=app.debug,use_reloader=use_debugger, host='0.0.0.0')
+  app.run()
